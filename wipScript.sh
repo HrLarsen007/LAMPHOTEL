2954cf307a6e4e6b5d4c3826482d8b6bde6f6a59
Morten Larsen <hrlarsen007@gmail.com>
2022-03-24 14:23:59 +0100

    test

--- wipScript.sh
+++ wipScript.sh
@@ -244,8 +244,8 @@ Q2="CREATE USER $localuser@'localhost' IDENTIFIED BY '$localpass';"
 Q3="GRANT ALL PRIVILEGES on $database.* TO $localuser@localhost;"
 Q4="FLUSH PRIVILEGES;"
 
-Q5="CREATE USER $remoteuser@'192.168.123.%' IDENTIFIED BY '$remotepass';"
-Q6="GRANT ALL PRIVILEGES ON $database.* TO $remoteuser@'192.168.123.%' WITH GRANT OPTION;"
+Q5="CREATE USER $remoteuser@'%' IDENTIFIED BY '$remotepass';"
+Q6="GRANT ALL PRIVILEGES ON $database.* TO $remoteuser@'%' WITH GRANT OPTION;"
 Q7="FLUSH PRIVILEGES;"
 
 SQL=${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}
