3739315a6234db60203ece38833faaaae67212da
Morten Larsen <hrlarsen007@gmail.com>
2022-04-19 12:29:16 +0200

    test

--- DockerTest.sh
+++ DockerTest.sh
@@ -32,8 +32,7 @@ END
 
 
 ## Installing a MariaDB the centos image
-sudo docker run -d -t --name $MySQLDBDocker centos
-docker run -d -t --name $MySQLDBDocker centos /bin/bash -c '
+sudo  run -d -t --name $MySQLDBDocker centos /bin/bash -c '
     sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
     sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
     yum -y update ; yum -y upgrade ; yum -y clean all
