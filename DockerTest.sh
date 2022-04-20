444e17fb5aa0581d83f60ec4705bfebbe3331790
Morten Larsen <hrlarsen007@gmail.com>
2022-04-20 12:52:07 +0200

    test

--- DockerTest.sh
+++ DockerTest.sh
@@ -1,50 +1,34 @@
 #!/bin/bash
 
 
-## Docker infomation
-MySQLDBDocker="MariaDB"
-MySQLIP=""
-ApachePHPDocker="ApachePHP"
-ApacheIP=""
-
-## DB information
-server_root="/var/www/html"
-wp_source="https://wordpress.org/latest.tar.gz"
-localuser="wpuser"
-remoteuser="maria"
+docker network create --subnet=172.18.0.0/24 LAMP-Network 
+docker run --name MariaDB -e MYSQL_ROOT_PASSWORD=Kode1234! --ip 172.18.0.100 -p 3308:3308 --network LAMP-Network -d docker.io/library/mariadb:10.3
+docker exec -it MariaDB /bin/bash -c '
+apt-get -y update ; apt-get -y upgrade ; apt-get -y clean all
 database="wpdatabase"
 table="wp_"
 
-remotepass=""
-localpass=""
+localuser="wpuser"
+localpass="Kode1234!"
+remoteuser="sshuser"
+remotepass="Kode1234!"
 
-: <<'END'
-## Installing Docker packages on the system
-sudo yum -y update ; yum -y upgrade ; yum -y clean all
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker
-sudo yum -y install docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo docker pull centos
-END
-  #sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
-  #sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+Q1="CREATE DATABASE $database;"
+Q2="CREATE USER $localuser@'localhost' IDENTIFIED BY '$localpass';"
+Q3="GRANT ALL PRIVILEGES on $database.* TO $localuser@localhost;"
+Q4="FLUSH PRIVILEGES;"
 
-## Installing a MariaDB the centos image
-docker run -it --name MariaDB centos/systemd /bin/bash -c '
-    yum -y update ; yum -y upgrade ; yum -y clean all
-    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-    yum install -y http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-    yum install -y yum-utils
-    yum install -y epel-release
-    yum install -y git
-    cd /
-    git clone https://github.com/hrlarsen007/LAMPHOTEL.git
-    sh LAMPHOTEL/DockerDB.sh
+Q5="CREATE USER $remoteuser@'%' IDENTIFIED BY '$remotepass';"
+Q6="GRANT ALL PRIVILEGES ON $database.* TO $remoteuser@'%' WITH GRANT OPTION;"
+Q7="FLUSH PRIVILEGES;"
+
+SQL=${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}
+mysql -u root -p -e "$SQL"
+
 '
-#sudo docker exec -it $MySQLDBDocker bash
 
+docker exec -it MariaDB /bin/bash -c '
+apt-get -y update ; apt-get -y upgrade ; apt-get -y clean all
+apt-get -y install wget ; apt-get -y install git
 
-#MySQLIP=`docker exec -it $MySQLDBDocker hostname -I`
+'
